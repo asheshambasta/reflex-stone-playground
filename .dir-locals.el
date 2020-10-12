1ec3d836d5a3d36ac526b0bc87dedbe836ca915e
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2020-10-12 08:31:26 +0200

    Some playing around with reflex stone.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+(
+ (nil . (
+         (haskell-mode-stylish-haskell-path . "brittany")
+         (dante-target . "reflex-stone")
+         (dante-methods . (new-impure-nix)))
+      )
+)
--- src/Main.hs
+++ src/Main.hs
@@ -1,40 +1,61 @@
-module Main where
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE TypeApplications #-}
+module Main
+  ( main
+  )
+where
 
-import qualified Data.Text as T
-import Reflex.Dom
+import           Control.Monad.Fix              ( MonadFix )
+import           Protolude
+import           Control.Monad                  ( void )
+import qualified Data.Text                     as T
+import           Reflex.Dom                    as RD
 
 main :: IO ()
-main =
-  mainWidget $ do
-    el "h1" $ text "reflex-stone"
-    clicked <- stoneButton
-    cnt <- foldDyn (+) (0 :: Int) $ 1 <$ clicked
-    elClass "p" "result" $ do
-      dyn_ $
-        ffor cnt $ \case
-          0 -> text "Go ahead and hit the stone."
-          n -> do
-            text $ T.pack (show n)
-            text " heads!"
+main = mainWidget $ do
+  el "h1" $ text "reflex-stone"
+  dText   <- buttonTextInput
+  clicked <- stoneButton dText
+  cnt     <- foldDyn (+) (0 :: Int) $ 1 <$ clicked
+  elClass "p" "result" $ dyn_ $ ffor cnt $ \case
+    0 -> text "Go ahead and hit the stone."
+    n -> text . T.unwords $ [show n, " heads!"]
 
-stoneButton :: DomBuilder t m => m (Event t ())
-stoneButton = do
-  let attr = ("style" =: "font-size: 200%;")
-  clickEvent $ elAttr' "button" attr stone
+buttonTextInput
+  :: (MonadHold t m, MonadFix m, DomBuilder t m) => m (Dynamic t T.Text)
+buttonTextInput = do
+  rec let send = keypress Enter input
+
+      input <-
+        RD.inputElement
+        $ def
+                    -- & (inputElementConfig_setValue .~ fmap _foo send)
+        & (  inputElementConfig_elementConfig
+          .  elementConfig_initialAttributes
+          .~ ("placeholder" =: "Stone!")
+          )
+
+  -- pure $ tag (RD.current $ _inputElement_value input) send
+  pure $ _inputElement_value input
+
+stoneButton
+  :: (MonadHold t m, MonadFix m, DomBuilder t m, PostBuild t m)
+  => Dynamic t T.Text
+  -> m (Event t ())
+stoneButton dText = do
+  let attr = "style" =: "font-size: 200%;"
+  -- dText <- RD.holdDyn "Stone!" eText
+  clickEvent $ elAttr' "button" attr (RD.dynText dText)
 
 stone :: DomBuilder t m => m ()
-stone =
-  text "🗿"
+stone = text "stone"
 
 -- | Get the click event on an element
 --
 -- Use as:
 --   clickEvent $ el' "a" ...
-clickEvent ::
-  ( DomBuilder t m,
-    HasDomEvent t target 'ClickTag
-  ) =>
-  m (target, a) ->
-  m (Event t ())
-clickEvent w =
-  fmap (fmap (const ()) . domEvent Click . fst) w
+clickEvent
+  :: (DomBuilder t m, HasDomEvent t target 'ClickTag)
+  => m (target, a)
+  -> m (Event t ())
+clickEvent = fmap (void . domEvent Click . fst)
